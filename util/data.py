35c0860ab52fd6576495495ca6fc7592836bf5be
Boschmann <johann.boschmann@atos.net>
2020-08-10 17:13:24 +0200

    First preprocessing and data splitting

--- util/data.py
+++ util/data.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 
 from pathlib import Path
 from typing import List
 from pandas import DataFrame
+from sklearn.preprocessing import MinMaxScaler
 
 
 def load_data(directory_path) -> List[DataFrame]:
@@ -14,6 +16,25 @@ def load_data(directory_path) -> List[DataFrame]:
     data_dir = Path(directory_path)
     sequences = []
     for path in data_dir.iterdir():
-        dataframe = pd.read_csv(path)
+        dataframe = pd.read_csv(path, sep=';')
         sequences.append(dataframe)
     return sequences
+
+
+def preprocess(df: DataFrame):
+    """
+    Take closes of time t
+    """
+    closes = np.array(df['closes'])
+    closes /= np.max(closes).reshape(-1,1)
+    return closes
+
+
+def split_train_test_validation(df: DataFrame):
+    """
+    Split data into training and test, for simplicity the first 5 years
+    (365 * 5 steps) are taken for training, rest for test and validation
+    """
+    train = 365 * 5
+    validation = 365
+    return df[:train], df[train:train+validation], df[train+validation:]
