1e2805e8b1c0ffefaa6b18611a151103749a46b5
Johann Boschmann <johannboschmann@hotmail.de>
2020-08-12 15:28:19 +0200

    Adds data windowing and unit tests for simple functionality.

---
+++ test/test_data.py
@@ -0,0 +1,27 @@
+import unittest
+import logging
+
+from util.data import load_data, split_train_test_validation, make_dataset, preprocess
+
+
+logger = logging.getLogger(__name__)
+
+
+class DataTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.assets = load_data('../data')
+        self.symbol = next(iter(self.assets.keys()))
+        self.asset = preprocess(self.assets[self.symbol])
+
+    def test_split_data(self):
+        df_train, df_validation, df_test = split_train_test_validation(self.asset)
+
+    def test_make_dataset(self):
+        df_train, df_validation, df_test = split_train_test_validation(self.asset)
+        dataset = make_dataset(df_train, sequence_length=252, sequence_stride=50)
+        print(dataset)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- util/data.py
+++ util/data.py
@@ -1,40 +1,89 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 from pathlib import Path
-from typing import List
-from pandas import DataFrame
-from sklearn.preprocessing import MinMaxScaler
+from typing import List, Dict, Any
+from pandas.core.frame import DataFrame
 
 
-def load_data(directory_path) -> List[DataFrame]:
+def load_data(directory_path) -> Dict[str, Any]:
     """
     Loads assets from csv files in given directory.
     :param directory_path: directory path containing the asset csvs
     :return:
     """
     data_dir = Path(directory_path)
-    sequences = []
+    assets = dict()
     for path in data_dir.iterdir():
+        symbol = path.name.split('_')[0]
         dataframe = pd.read_csv(path, sep=';')
-        sequences.append(dataframe)
-    return sequences
+        assets[symbol] = dataframe
+    return assets
 
 
 def preprocess(df: DataFrame):
     """
     Take closes of time t
     """
-    closes = np.array(df['closes'])
-    closes /= np.max(closes).reshape(-1,1)
-    return closes
+    clean = df.copy()
+    clean.pop('date')
+    return clean
 
 
 def split_train_test_validation(df: DataFrame):
     """
     Split data into training and test, for simplicity the first 5 years
-    (365 * 5 steps) are taken for training, rest for test and validation
+    (252 * 5 steps) are taken for training, rest for test and validation
     """
-    train = 365 * 5
-    validation = 365
+    train = 275 * 6
+    validation = 275 * 2
     return df[:train], df[train:train+validation], df[train+validation:]
+
+
+def make_dataset(df: DataFrame, sequence_length: int, sequence_stride=1, shift=1, columns=None) -> tf.data.Dataset:
+    """
+    Creates a dataset with
+    :param df: data frame containing data to process
+    :param sequence_length: length of sequence, inclusive prediction
+    :param columns:
+    :param sequence_stride:
+    :param shift: determines the shift of the prediction (t1, .. , tn) -> t+shift
+    :return:
+    """
+    data = np.array(df, dtype=np.float32)
+    ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+        data=data,
+        targets=None,
+        sequence_length=sequence_length,
+        sequence_stride=sequence_stride,
+        shuffle=True,
+        batch_size=32,)
+    column_indices = {name: i for i, name in enumerate(df.columns)}
+    ds = ds.map(split_sequence(sequence_length=sequence_length,
+                               prediction_length=sequence_length,
+                               shift=shift,
+                               label_column_indices=[column_indices['close']]))
+    return ds
+
+
+def split_sequence(sequence_length: int, prediction_length: int, shift: int, label_column_indices: List[int]):
+    """
+    splits a sequence into a input and a label part
+    :param sequence_length: sequence length of input
+    :param prediction_length: sequence length of prediction
+    :param label_column_indices: columns that are being predicted
+    :param shift: determines the shift of the prediction (t1, .. , tn) -> t+shift
+    :return:
+    """
+    def _split_sequence(dataset: tf.data.Dataset):
+        inputs = dataset[:, slice(0, sequence_length - shift), :]
+        labels = dataset[:, slice(sequence_length - prediction_length, None), :]
+        labels = tf.stack(
+            [labels[:, :, idx] for idx in label_column_indices],
+            axis=-1)
+        inputs.set_shape([None, sequence_length - shift, None])
+        labels.set_shape([None, sequence_length - prediction_length, None])
+        print(inputs, labels)
+        return inputs, labels
+    return _split_sequence
